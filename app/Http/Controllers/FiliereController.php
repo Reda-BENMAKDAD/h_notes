c77ca93161fa2d67954abcdcc8fe86c693124da1
aminedjohar <aminedjohar@gmail.com>
2023-04-26 10:35:52 +0100

    I added moduleController and filliereControler

---
+++ app/Http/Controllers/FiliereController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FiliereController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $filieres = Filiere::all();
+        return view('filieres.index', ['filieres'=>$filieres]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('filieres.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+      
+        $nom=$request->input('nom');
+       
+          $validation=Validator::make($request->all(),
+          [
+            'nom'=>'required|max:200',
+            
+          ],
+          [
+           'nom.required'=>'Erreur le libelle est obligatoires' ,
+           'nom.max'=>'Erreur le libelle ne doit pas dépassé 200 caractères',
+          ]);
+
+          if($validation->fails()){
+            return back()->withErrors($validation->errors())->withInput();
+           }
+           Filiere::create($request->post());
+
+      return redirect()->route('filiers.index')->with('message',"bien ajouté");
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $filier=Filiere::findorFail($id);
+        return view('filiers.show',['filier'=>$filier]);
+    }
+
+   
+    public function edit(string $id)
+    {
+        $filier=Filiere::findorFail($id);
+        return view('filiers.edit',['filier'=>$filier]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // $validation=Validator::make($request->all(),
+        // [
+        //     'libelle'=>'required|max:25',
+        //     'infos'=>'required'
+        
+        // ],
+        // [
+        //     'libelle.required'=>'le libelle est obligatoire',
+        //     'libelle.max'=>'le libelle ne doit pas dépassé 25 caractères',
+        //     'infos.required'=>'les infos est obligatoire'
+
+        // ]
+        // );
+
+        // if($validation->fails()){
+        //     return back()->withErrors($validation->errors())->withInput();
+        //    }
+
+           $filier=Filiere::findorFail($id);
+           $filier->nom=$request->input('libelle');
+           $filier->save();
+           return redirect()->route('filiers.index')->with('message','la filiere est bien modifié');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $filier=Filiere::findorFail($id);
+        $filier->delete();
+        return redirect()->route('filiers.index')->with('message','la filiere a bien été suppprimée');
+    }
+}
---
+++ app/Http/Controllers/ModuleController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Module;
+
+use Illuminate\Http\Request;
+
+class ModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $module = Module::all();
+        return view('modules.index', ['module'=>$module]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('modules.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+      
+        $nom=$request->input('nom');
+        $masseHoraire=$request->input('masseHoraire');
+    
+          $validation=Validator::make($request->all(),
+          [
+            'nom'=>'required|max:200',
+            'masseHoraire'=>'required'
+          ],
+          [
+           'nom.required'=>'Erreur le libelle est obligatoires' ,
+           'nom.max'=>'Erreur le libelle ne doit pas dépassé 200 caractères',
+           'masseHoraire.required'=>'les infos est obligatoires'
+          ]);
+
+          if($validation->fails()){
+            return back()->withErrors($validation->errors())->withInput();
+           }
+           Module::create($request->post());
+
+      return redirect()->route('modules.index')->with('message',"bien ajouté");
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $module=Module::findorFail($id);
+        return view('modules.show',['module'=>$module]);
+    }
+
+   
+    public function edit(string $id)
+    {
+        $module=Module::findorFail($id);
+        return view('modules.edit',['modules'=>$module]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // $validation=Validator::make($request->all(),
+        // [
+        //     'libelle'=>'required|max:25',
+        //     'infos'=>'required'
+        
+        // ],
+        // [
+        //     'libelle.required'=>'le libelle est obligatoire',
+        //     'libelle.max'=>'le libelle ne doit pas dépassé 25 caractères',
+        //     'infos.required'=>'les infos est obligatoire'
+
+        // ]
+        // );
+
+        // if($validation->fails()){
+        //     return back()->withErrors($validation->errors())->withInput();
+        //    }
+
+           $module=Module::findorFail($id);
+           $module->nom=$request->input('nom');
+           $module->masseHorraire=$request->input('masseHorraire');
+           $module->save();
+           return redirect()->route('modules.index')->with('message','le module est bien modifié');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $module=Module::findorFail($id);
+        $module->delete();
+        return redirect()->route('modules.index')->with('message','le module a bien été suppprimée');
+    }
+}
