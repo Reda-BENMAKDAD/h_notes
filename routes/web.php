3e2d6b1a36486d29dfe6826cf6e43278ec0ea600
aminedjohar <aminedjohar@gmail.com>
2023-04-26 16:31:58 +0100

    I added CRUD fonctionalities for filiere in views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FiliereController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('filiers', FiliereController::class);
 
 require __DIR__.'/auth.php';
